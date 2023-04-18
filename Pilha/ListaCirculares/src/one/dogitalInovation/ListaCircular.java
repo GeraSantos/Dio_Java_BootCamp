f2fc6803e1367331e8ba5eb9a362bb94b47c4ee8
Geraldo Santos <geraldo.gsantos12@gmail.com>
2023-04-17 23:17:49 -0300

    Implementação da classe ListaCircular - metodos getNo() e get()

--- Pilha/ListaCirculares/src/one/dogitalInovation/ListaCircular.java
+++ Pilha/ListaCirculares/src/one/dogitalInovation/ListaCircular.java
@@ -1,5 +1,35 @@
 package one.dogitalInovation;
 
-public class ListaCircular {
-
+public class ListaCircular <T> {
+	
+	private No<T> cabeca;
+	private No<T> cauda;
+	private int tamanhoLista;
+	
+	private No<T> getNo(int index){
+		if(this.isEmpty())
+			throw new IndexOutOfBoundsException("A lista está vazia!");
+		
+		if(index == 0) {
+			return this.cauda;
+		}
+		
+		No<T> noAuxiliar = this.cauda;
+		for(int i = 0; (i < index) && (noAuxiliar != null); i++) {
+			noAuxiliar = noAuxiliar.getNoProximo();
+		}
+		
+		return noAuxiliar;
+		
+	}
+	
+	public boolean isEmpty() {
+		return this.tamanhoLista == 0 ? true : false;
+	}
+	
+	public int size() {
+		return this.tamanhoLista;
+	}
+	
+	
 }
